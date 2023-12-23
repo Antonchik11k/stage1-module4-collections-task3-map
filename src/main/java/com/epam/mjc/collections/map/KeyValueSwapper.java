6ed05db0ab2d6a3e6ebad3e99acd467f00089a71
Anton Kolotusha <qq102096@gmail.com>
2023-12-23 14:22:01 +0100

    Solved

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,28 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swappedMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            String value = entry.getValue();
+            Integer currentKey = entry.getKey();
+
+            // Check if the value already exists in the swapped map
+            if (swappedMap.containsKey(value)) {
+                // Compare the current key with the one already in the map
+                // Update if the current key is smaller
+                if (currentKey < swappedMap.get(value)) {
+                    swappedMap.put(value, currentKey);
+                }
+            } else {
+                // If the value is not in the map yet, just put it
+                swappedMap.put(value, currentKey);
+            }
+        }
+        return swappedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,19 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        if(sentence.isEmpty() || sentence == null){
+            return new HashMap<>();
+        }
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        // Split words by non-word characters and eliminate punctuation
+        var words = sentence.toLowerCase().split("\\W+");
+        for(String word : words){
+            hashMap.put(word, hashMap.getOrDefault(word, 0) + 1);
+        }
+        return hashMap;
     }
 }
